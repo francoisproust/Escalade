191d0f1ee60b313a8c1b8288d2d9998c02c5f9fc
francoisproust <francois.proust.72@gmail.com>
2020-01-17 16:02:15 +0100

    passage en base H2 pour gagner du temps avec le mentor

--- src/main/java/proust/dev/escalade/Escalade.java
+++ src/main/java/proust/dev/escalade/Escalade.java
@@ -1,7 +1,6 @@
 package proust.dev.escalade;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,7 +14,7 @@ import java.sql.SQLException;
 @EnableSwagger2
 public class Escalade {
     public static void main(String[] args) throws SQLException {
-        HibernateUtils.getSessionFactory().openSession();
+        //HibernateUtils.getSessionFactory().openSession();
         System.out.println("tout va bien!!!!!!");
         SpringApplication.run(Escalade.class,args);
         System.out.println("SPRING OKKKKKKKKKKKKKKKKKKKKKKKKKKK");
--- src/main/java/proust/dev/hibernate/controller/UtilisateurController.java
+++ src/main/java/proust/dev/hibernate/controller/UtilisateurController.java
@@ -1,46 +1,5 @@
 package proust.dev.hibernate.controller;
 
-import proust.dev.hibernate.dao.UtilisateurDao;
-import proust.dev.hibernate.entities.Utilisateur;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
-@Api(description = "Gestion des utilisateurs")
-@RestController
 public class UtilisateurController {
-   @Autowired
-   private UtilisateurDao utilisateurDao;
-
-    ///Utilisateurs
-    @ApiOperation(value = "Récupère la liste des utilisateurs")
-    @GetMapping(value = "Utilisateurs")
-    public List<Utilisateur> listeUtilisateurs(){
-        //return new ArrayList<>();
-        return utilisateurDao.findAll();
-    }
-
-    ///Créer utilisateurs
-    @ApiOperation(value = "Ajoute un utilisateur")
-    @PostMapping(value = "/Utilisateurs")
-    public ResponseEntity<Void> ajouterUtilisateur(@RequestBody Utilisateur utilisateur){
-        Utilisateur utilisateur1 = utilisateurDao.save(utilisateur);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{userId}")
-                .buildAndExpand(utilisateur1.getUserId())
-                .toUri();
-        return ResponseEntity.created(location).build();
-    }
 
 }
